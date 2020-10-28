f0ceaa850f4fcba92fb1537c8284eef729a7d7ff
James Darling <jamesdarling@microsoft.com>
2020-10-28 17:17:16 -0500

    Added test runner console app.

---
+++ FantasyFootballDashboard.ConsoleRunner/Program.cs
@@ -0,0 +1,14 @@
+﻿using FantasyFootballDashboard.ConsoleRunner.TestHarness;
+using System;
+
+namespace FantasyFootballDashboard.ConsoleRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var testResult = CbsTester.ExecuteTest();
+            Console.WriteLine(testResult);
+        }
+    }
+}
---
+++ FantasyFootballDashboard.ConsoleRunner/TestHarness/CbsTester.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace FantasyFootballDashboard.ConsoleRunner.TestHarness
+{
+    public static class CbsTester
+    {
+        public static string ExecuteTest()
+        {
+            return "CBS Test was successful!";
+        }
+    }
+}
