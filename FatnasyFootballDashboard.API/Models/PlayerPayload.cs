578e0c807985e8b77baa0c1bde13407c8f2330e3
James Darling <jamesdarling@microsoft.com>
2020-11-12 07:13:45 -0600

    API players endpoint cleanup

---
+++ FatnasyFootballDashboard.API/Models/PlayerPayload.cs
@@ -0,0 +1,15 @@
+﻿using FantasyFootballDashboard.Models;
+using System.Collections.Generic;
+
+namespace FatnasyFootballDashboard.API.Models
+{
+    public class PlayerPayload
+    {
+        public PlayerPayload(List<Player> players)
+        {
+            Players = players;
+        }
+
+        public List<Player> Players { get; set; }
+    }
+}
