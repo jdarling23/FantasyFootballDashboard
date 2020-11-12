826e7918fef704fd48636e972fc26f32b9dcddc2
James Darling <jamesdarling@microsoft.com>
2020-11-12 07:16:39 -0600

    Created unit test project and added some solution folders for organization.

---
+++ FantasyFootballDashboard.Test.Unit/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FantasyFootballDashboard.Test.Unit
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
